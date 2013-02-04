c8c10512699438e8265b00e937fa8e13bb9ae1e8
Gary Johnson <jazzbassjohnson@gmail.com>
2013-02-04 11:05:58 -0800

    Finish Layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
+  def home#typed manually
+  end
+  def help#typed manually
+  end
+  def about#typed manually
+  end
   def contact
   end
 end
